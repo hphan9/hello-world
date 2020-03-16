3b2af2cb34251f0319a61e0449de3216d65cef29
Emily <55158848+hphan9@users.noreply.github.com>
2020-03-16 00:16:17 -0500

    Update picture.js

--- picture.js
+++ picture.js
@@ -1,11 +1,5 @@
 (function(){
 
-window.onload= function(){
-let header= document.querySelector("#header");
-let mountain= document.createElement('img');
-mountain.src= '../images/albertaMountain.JPG';
-mountain.alt= 'Mountain picture';
-header.appendChild(mountain);
-};
+
 
 })();
