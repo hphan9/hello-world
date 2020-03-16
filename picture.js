c1a786321e958878a5e4f91c0827e4a2cc5b1576
Emily <55158848+hphan9@users.noreply.github.com>
2020-03-16 00:00:54 -0500

    Create picture.js

---
+++ picture.js
@@ -0,0 +1,11 @@
+(function(){
+
+window.onload= function(){
+let header= document.querySelector("#header");
+let mountain= document.createElement('img');
+mountain.src= '../images/albertaMountain.JPG';
+mountain.alt= 'Mountain picture';
+header.appendChild(mountain);
+};
+
+})();
