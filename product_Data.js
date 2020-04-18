ae4d7d99b7683d70aeb92c9225bfc97721215747
Emily <hphan9@myseneca.ca>
2020-04-17 22:12:32 -0500

    Delete product_Data.js

--- product_Data.js
+++
@@ -1,8 +0,0 @@
-/* Photo by Mr TT on Unsplash -1st
-Photo by gustavo centurion on Unsplash
-Photo by The Creative Exchange on Unsplash
-Photo by Markus Spiske on Unsplash
-Photo by Vlad Kutepov on Unsplash
-Photo by Katerina on Unsplash
-Photo by V Srinivasan on Unsplash
-*/
